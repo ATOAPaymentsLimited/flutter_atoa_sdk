3f068ebfb3d319ddf1691cedd5d5b1f945a01ea3
Aman Nanda <aman+github@paywithatoa.co.uk>
2024-01-17 14:22:03 +0530

    feat: example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:atoa_sdk/atoa_sdk.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:regal/regal.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,14 +11,42 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+  MaterialApp getApp(BuildContext context) {
+    return MaterialApp(
+      theme: kThemeData,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      home: Builder(builder: (context) {
+        return Scaffold(
+          body: const Center(
+            child: Text('Hello World!'),
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: () {
+            AtoaSdk.show(context);
+          }),
+        );
+      }),
     );
   }
+
+  @override
+  Widget build(BuildContext context) => ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        child: FutureBuilder<SharedPreferences>(
+          future: SharedPreferences.getInstance(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return RegalThemeProvider(
+                prefs: snapshot.data!,
+                builder: (context, themeMode, child) => Builder(
+                  builder: (context) {
+                    return getApp(context);
+                  },
+                ),
+              );
+            }
+            return getApp(context);
+          },
+        ),
+      );
 }
