21932a610d7366d42e57f15c50eb119bd3315686
Aman Nanda <aman+github@paywithatoa.co.uk>
2024-01-17 14:16:09 +0530

    fix: lint

--- packages/atoa_core/lib/src/models/payment_auth_payment_request/payment_auth_payment_request.dart
+++ packages/atoa_core/lib/src/models/payment_auth_payment_request/payment_auth_payment_request.dart
@@ -1,4 +1,3 @@
-import 'package:atoa_core/src/models/payer_bank_details/payer_bank_details.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'payment_auth_payment_request.freezed.dart';
